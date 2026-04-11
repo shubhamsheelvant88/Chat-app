d3b6ba5c660d75a42b3905e750fb3f364230321c
shubham <shubhamsheelvant@gmail.com>
2026-04-11 13:22:45 +0530

    Chat app

---
+++ app.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" , () => {
+    let inp = document.querySelector("input");
+
+    let message = inp.value;
+
+    let div = document.createElement("div");
+
+    let chatBox = document.querySelector("#chat");
+
+    div.innerText = message; // div inside the chat div
+
+    chatBox.appendChild(div); // append in the chat div
+
+    inp.value = "";
+})
